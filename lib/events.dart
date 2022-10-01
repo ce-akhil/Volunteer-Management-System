623b5a6aa38d7d2c7825b1c2121367c8530980a3
akhi-dev <72202225+akhi-dev@users.noreply.github.com>
2022-10-01 16:00:13 +0530

    Update events.dart

--- lib/events.dart
+++ lib/events.dart
@@ -12,7 +12,7 @@ class _EventsState extends State<Events> {
     return  Scaffold(
       appBar: AppBar(
         title: Text(
-          'Upcoming Events',
+          'Events in review',
         ),
       ),
       body: SafeArea(
@@ -22,10 +22,10 @@ class _EventsState extends State<Events> {
               Container(
                 decoration: BoxDecoration(
                   border:Border.all(
-                    width: 2.0,
+                    width: 4.0,
                   ),
                 ),
-                height: 200.0, // Always have width in divisible of 8
+                height: 240.0, 
                 child: Padding(
                   padding: EdgeInsets.all(7),
                   child: Stack(children: <Widget>[
@@ -49,21 +49,21 @@ class _EventsState extends State<Events> {
                             Container(
                               decoration: BoxDecoration(
                               ),
-                              height: 50.0,
+                              height: 56.0,
                               child: ListTile(
                                   leading: Icon(FontAwesomeIcons.searchLocation),
                                   title: Text('Location'),
                                   trailing: Text('1 April 2020')
                               ),
                             ),
-                            SizedBox(height: 10),
+                          // do not add sized box
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: <Widget>[
                                 Container(
                                   decoration: BoxDecoration(
                                   ),
-                                  height: 35.0,
+                                  height: 32.0,
                                   child: Padding(
                                     padding:  EdgeInsets.all(8.0),
                                     child: Text('Status: Pending'
@@ -78,9 +78,9 @@ class _EventsState extends State<Events> {
                                     child: RaisedButton(
                                       child: Text('Register',
                                       style: TextStyle(
-                                        color: Color(0xFF512DA8),
+                                        color: Colors.blue, //create colorscheme
                                       ),),
-                                      onPressed: null
+                                      onPressed: (){} // do not write null
                                     ),
                                   ),
                                 ),
@@ -93,9 +93,7 @@ class _EventsState extends State<Events> {
                   ]),
                 ),
               ),
-              SizedBox(
-                height: 20,
-              ),
+              
               Container(
                 decoration: BoxDecoration(
                   border:Border.all(
@@ -122,7 +120,7 @@ class _EventsState extends State<Events> {
                                 trailing: Text('Mumbai'),
                               ),
                             ),
-                            SizedBox(height: 10),
+                           
                             Container(
                               decoration: BoxDecoration(
                               ),
@@ -210,7 +208,7 @@ class _EventsState extends State<Events> {
                                   trailing: Text('10 April 2020')
                               ),
                             ),
-                            SizedBox(height: 10),
+                            SizedBox(height: 10), // give padding instead
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: <Widget>[
